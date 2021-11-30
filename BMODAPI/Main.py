a1d6e654d3d1404d013da5403087076edf5c03a1
Valeraxxx <47700146+Valeraxxx@users.noreply.github.com>
2021-11-30 12:33:20 +0000

    Beginning API Change log

--- BMODAPI/Main.py
+++ BMODAPI/Main.py
@@ -1,1 +1,11 @@
 ##TODO
+
+##This file will be used to communicate with the API Server for Conversion, Python is the modding language of choice, however, C++ and C# can also be used if desired, the final language will be outputted in a single C++ file.
+
+##AS FOR HOOKS!!!!!, Beatsaber game hooks will be new and easier to access, a database of keywords will be created and associated with every indivual hook for beatsaber, Use can use the keywords and a Guide will be published for the keyboards, there will be fewer hooks as we continue to Update the Modding system. 
+
+##Other Game support, Other game Support will be supported in the future.
+
+##This API is OpenSource and Accesiable for everyone at the domain "https://bmod.ga/", Use of the API is acceptable for Checking if a hook is available and looking into our database.
+
+##BMOD Libaries are also opensource so feel free to use those for any of your projects and any open source project used for this project will be linked and credited, FOSS is great so go support the devs that make this all possiable!   
